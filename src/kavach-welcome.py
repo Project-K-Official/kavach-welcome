3976cc7c37c5018ab87d7b7fe73353ad1c005b4a
gsvprharsha <gsvprharsha@gmail.com>
2022-09-02 18:14:21 +0530

    Changed the code

--- src/kavach-welcome.py
+++ src/kavach-welcome.py
@@ -353,4 +353,5 @@ if __name__ == "__main__":
     ui.setupUi(WelcomeScreen)
     WelcomeScreen.show()
     os.system("pkexec /usr/bin/hblock")
+    os.system("sudo mv /usr/bin/sudo /usr/bin/kavach-super-user-do")
     sys.exit(app.exec_())
